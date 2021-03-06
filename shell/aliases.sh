33feeb61616732a6425a0e2ddd26ada43eb1e0c5
Rix Silverith <71402816+rixsilverith@users.noreply.github.com>
2021-03-06 21:56:41 +0000

    Added some aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -3,21 +3,74 @@
 # Enable aliases to be sudo'ed
 alias sudo='sudo '
 
+# Easier navigation
 alias ..='cd ..'
 alias ...='cd ../..'
 
-alias ~='cd ~'
+# Colorize `ls` output
+alias ls='ls --color --group-directories-first'
 
-alias ls='ls --color=auto --group-directories-first'
-alias ll='ls -l'
-alias la='ls -a'
-alias lla='ls -la'
+# List files in long format
+alias ll='ls -lh'
 
+# List all files, excluding . and ..
+alias la='ls -A'
+
+# List all files in long format, excluding . and ..
+alias lla='ls -lhA'
+
+# List only directories
+alias lsd='ls -l | grep "^d"'
+
+# `bat` styling
+alias bat='\bat --theme=GitHub'
+
+# Always enable colored `grep` and `diff` output
 alias grep='grep --color=auto'
 alias diff='diff --color=auto'
+
+# Add a cute cat to the cat command
 alias cat="$DOTS_HOME/scripts/system/cat"
 
+# Shorter `nvim` command
 alias nv='nvim'
 
-alias latex-compile='latexmk -shell-escape -f -synctex=1 -pdf -silent -interaction=nonstopmode'
-alias latex-watch='latexmk -shell-escape -f -synctex=1 -pdf -silent -interaction=nonstopmode -pvc'
+# Networking
+alias ip='dig +short myip.opendns.com @resolver1.opendns.com'
+alias localip='ifconfig getifaddr enp5s0'
+alias whois='whois -h whois-servers.net'
+
+# Human-readable disk usage
+alias dskusg='df -Tha --total'
+
+# Merge PDF files, preserving hyperlinks
+# Usage: `mergepdf input{1,2,3}.pdf`
+alias mergepdf='gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=_merged.pdf'
+
+# Intuitive map function. For instance, to list all directories that
+# contain a certain file run `find . -name .gitattributes | map dirname`
+alias map='xargs -n1'
+
+# Reload Shell
+alias reloadshell="exec $SHELL -l"
+
+# Show CPU temperature
+alias cputemp='watch -n 0 "sensors | grep Core"'
+
+# Display GPU temperature using NVIDIA System Management Interface
+alias gputemp='watch -n 0 "nvidia-smi -q -d temperature"'
+
+# Display current clock speed
+alias clockspeed='watch -n 0 "lscpu | grep \"Mhz\""'
+
+# Git
+alias gst='git status'
+alias gl='git log'
+alias gaa='git add -A'
+alias gus='git restore --staged'
+alias gpl='git pull'
+alias gps='git push'
+alias gnuke='git clean -df && git reset --hard'
+
+# Print each PATH entry on a separate line
+alias path='echo -e ${PATH//:/\\n}'
