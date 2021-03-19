4308d909a3aede6b8b950a4174509891bac3b1cc
Rix Silverith <71402816+rixsilverith@users.noreply.github.com>
2021-03-19 18:29:03 +0000

    Fixed autostart script

--- wm/autostart.sh
+++ wm/autostart.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 
 # Set keymap to Spanish layout
-setxkeymap es &
+setxkbmap es
 
 # Restore wallpaper
-feh --bg-scale --no-fehbg $DOTS_HOME/misc/walls/.wall.jpg &
+feh --bg-scale --no-fehbg $DOTS_HOME/misc/walls/.wall.jpg
 
 # Restore cursor theme
-xsetroot -cursor_name left_ptr &
+xsetroot -cursor_name left_ptr
 
 # Automounter
 udiskie &
