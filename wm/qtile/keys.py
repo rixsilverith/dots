e6668cf5b2caca549f0b43eac60fa4d69a19b445
Rix Silverith <71402816+rixsilverith@users.noreply.github.com>
2021-04-01 09:59:38 +0200

    Added 'dot system night_mode' script

--- wm/qtile/keys.py
+++ wm/qtile/keys.py
@@ -32,8 +32,7 @@ keys = [Key(key[0], key[1], *key[2:]) for key in [
     ([mod], "b", lazy.spawn("firefox")),
     ([mod], "e", lazy.spawn("thunar")),
     ([mod], "Return", lazy.spawn("alacritty")),
-    ([mod], "r", lazy.spawn("redshift -O 3700")),
-    ([mod, "shift"], "r", lazy.spawn("redshift -x")),
+    ([mod], "r", lazy.spawn("dot system night_mode toggle")),
 
     # Multimedia keys
     ([], "XF86AudioLowerVolume", lazy.spawn("pamixer --decrease 5")),
