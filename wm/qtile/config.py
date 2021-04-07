32be32bc2b77d29f9f69c3c38f926e7191ac6805
Rix Silverith <71402816+rixsilverith@users.noreply.github.com>
2021-04-07 09:44:05 +0200

    Improved Qtile groups displacement and keys assigned to them

--- wm/qtile/config.py
+++ wm/qtile/config.py
@@ -1,5 +1,3 @@
-from libqtile.dgroups import simple_key_binder
-
 from keys import mod, keys
 from groups import groups
 from layouts import layouts, floating_layout
@@ -8,14 +6,11 @@ from screens import screens
 from mouse import mouse
 
 main = None
-
-# auto bind keys to dgroups mod+(1 to 9)
-dgroups_key_binder = simple_key_binder(mod)
-
+dgroups_key_binder = None
 dgroups_app_rules = []
-follow_mouse_focus = True
+follow_mouse_focus = False
 bring_front_click = False
-cursor_warp = True
+cursor_warp = False
 auto_fullscreen = True
 focus_on_window_activation = 'urgent'
 wmname = 'LG3D'
--- wm/qtile/groups.py
+++ wm/qtile/groups.py
@@ -1,10 +1,21 @@
 from libqtile.config import Key, Group, Match
 from libqtile.command import lazy
+from keys import mod, keys
 
 groups = [
-    Group('other', label='  '),
+    Group('term', label='  ', init=True, persist=True, matches=[Match(wm_class=['Alacritty'])], position=1),
+    Group('browser', label='  ', init=True, persist=True, matches=[Match(wm_class=['firefox'])], position=2),
+    Group('discord', label='  ', init=True, persist=True, matches=[Match(wm_class=['discord'])], position=3),
 
-    Group('term', label='  ', init=False, persist=False, matches=[Match(wm_class=['Alacritty'])], position=1),
-    Group('browser', label='  ', init=False, persist=False, matches=[Match(wm_class=['firefox'])], position=2),
-    Group('discord', label='  ', init=False, persist=False, matches=[Match(wm_class=['discord'])], position=3),
+    Group('other', label='  '),
 ]
+
+for i, group in enumerate(groups):
+    current_key = str(i + 1)
+    keys.extend([
+        # Switch to group N. Here toggle=False prevents the group from being toggle
+        # with the last used it's already on the screen
+        Key([mod], current_key, lazy.group[group.name].toscreen(toggle=False)),
+        # Send window to group N
+        Key([mod, "shift"], current_key, lazy.window.togroup(group.name))
+    ])
