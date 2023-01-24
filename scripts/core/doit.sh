e8f39e387a338e841a16fbc5c29e78c2f6c73813
Rix Silverith <71402816+rixsilverith@users.noreply.github.com>
2023-01-24 23:35:16 +0100

    Renamed `dot` command to `doit` to avoid conflicts with other commands

--- scripts/core/doit.sh
+++ scripts/core/doit.sh
@@ -9,7 +9,8 @@ if ! ${DOT_MAIN_SOURCED:-false}; then
   readonly DOT_MAIN_SOURCED=true
 fi
 
-dot::alias() {
+# resolve a script context alias
+doit::context_alias() {
   local -r context="$1"
   case $context in
     fs)   echo filesystem ;;
@@ -24,27 +25,23 @@ dot::alias() {
   esac
 }
 
-dot::list_scripts() {
+doit::list_scripts() {
   contexts=$(find "$DOTS_HOME/scripts" -mindepth 2 -maxdepth 2 -executable -type f | grep -v core)
   printf "%s" "$contexts" | sort -u
 }
 
-dot::script_exists() {
+doit::script_exists() {
   [[ -x "$DOTS_HOME/scripts/${1}/${2}" ]]
 }
 
-dot::completion() {
+# if 'doit' is run without arguments, list all available scripts
+doit::completion() {
   local paths="$1"
 
   script="$(
     echo "$paths" |
       xargs -I % sh -c 'echo "$(basename $(dirname %)) $(basename %)"' |
-      fzf \
-        --layout reverse-list \
-        --height 10% \
-        --min-height 1 \
-        --cycle \
-        --pointer '➜' \
+      fzf --layout reverse-list --height 10% --min-height 1 --cycle --pointer '➜' \
         --color 'hl:255,hl+:255,pointer:255:bold,marker:255,info:248,prompt:255,bg+:-1' \
         --preview '"$DOTS_HOME/bin/dot" $(echo {} | cut -d" " -f 1) $(echo {} | cut -d" " -f 2) -h'
   )"
@@ -52,16 +49,13 @@ dot::completion() {
   printf "%s" "$script"
   read -r args
 
-  "$DOTS_HOME/bin/dot" $script $args
+  "$DOTS_HOME/bin/doit" $script $args
 }
 
-dot::show_help() {
-  echo
-  echo -e "$(tput bold)An entry point for all scripts inside this dotfiles$(tput sgr0)"
-  echo -e "\nUsage: $(tput bold)dot <context> <script> [<args>...]\n"
-  echo -e "\t$(tput bold)dot self update$(tput sgr0)      Check for updates"
-  echo -e "\nOptions:"
-  echo -e "\t$(tput bold)dot -h$(tput sgr0)                      Show this help message"
-  echo -e "\t$(tput bold)dot <context> <script> -h$(tput sgr0)   Show help for a command\n"
+doit::show_help() {
+  echo -e "doit script entrypoint"
+  echo -e "Usage: doit <context> <script> [<args>...]\n"
+  echo -e "Running \`doit <context> <script> -h\` will show specific help for a given script."
+  exit 1
 }
 
