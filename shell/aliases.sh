36c8f8bab9918345bd35d910031a890a4ebe114a
Rix Silverith <71402816+rixsilverith@users.noreply.github.com>
2023-06-01 23:33:58 +0200

    Shell aliases clean up

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,85 +1,70 @@
 # `aliases.sh` defines some aliases that are shared across the
 # bash and zsh shells.
 
-# Enable aliases to be sudo'ed
-alias sudo='sudo '
-# Intuitive map function. For instance, to list all directories that
-# contain a certain file run `find . -name .gitattributes | map dirname`
+alias sudo='sudo ' # enable aliases to be sudo'ed
+
+# intuitive map function. ex: list all directories that
+# contain a certain file `find . -name <file> | map dirname`
 alias map='xargs -n1'
-# Simpler command to give execution permissions
-alias chmox='chmod +x'
-# Simpler command to open files
-alias open='xdg-open'
-# Shortcut for extracing compressed files
-alias ext='dot filesystem extract'
-# Human-readable disk usage
-alias dskusg='df -Tha --total'
-# Reload current shell
-alias reloadshell="exec $SHELL -l"
-# Print each $PATH entry on a separate line
-alias path='echo -e ${PATH//:/\\n}'
-# Simpler copy to clipboard
+
+# safe and verbose chmod and chown
+# --preserve-root prevents operating recursively on /
+alias chmod='chmod --preserve-root -v'
+alias chown='chown --preserve-root -v'
+
+has safe-rm && alias rm='safe-rm' # safe rm
+
+alias ext="$DOTS_HOME/scripts/filesystem/extract"
+alias path='echo -e ${PATH//:/\\n}' # print each $PATH entry on a separate line
+
 has xclip && alias clip='xclip -sel clip'
-# Shorter `nvim` command
 has nvim && alias nv='nvim' || alias nv='vim'
-# Ensure rsync uses (a)rchive mode, compression (z) and (v)erbose mode
+
+# ensure rsync uses (a)rchive mode, compression (z) and (v)erbose mode
 has rsync && alias rsync='rsync -azv --delete --progress'
-# Show public IP address
 has curl && alias publicip='curl https://ipinfo.io/ip && echo'
-# Merge PDF files, preserving hyperlinks
-# Usage: `mergepdf input{1,2,3}.pdf`
+
+# merge PDF files, preserving hyperlinks
+# usage: `mergepdf input{1,2,3}.pdf`
 has gs && alias mergepdf='gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=_merged.pdf'
 
 if has valgrind; then
-  # Use Valgrind Memcheck tool to search for memory leaks in a program, tracing
+  # use Valgrind Memcheck tool to search for memory leaks in a program, tracing
   # the origins of all uninitialised values that may cause an error. `man valgrind`
   # for more information.
   alias vgd='valgrind --tool=memcheck --leak-check=full --track-origins=yes'
-  # Use Valgrind Massif tool to profile and log heap memory and stack usage of a
+  # use Valgrind Massif tool to profile and log heap memory and stack usage of a
   # program. `man valgrind` for more information.
   alias vgdlg='valgrind --tool=massif --stacks=yes'
-fi;
+fi
 
-# Enable colored `ls` output
+alias df='df --print-type --human-readable' # df -Th: human-readable disk usage
+alias du='du --human-readable'
+
+alias mkdir='mkdir -pv'
+# safer versions of some commands
+alias mv='mv -iv'
+alias cp='cp -iv'
+alias rm='rm -Iv --one-file-system' # -I less intrusive than -i
+
 alias ls='ls --color --group-directories-first'
-# List files in long format
-alias ll='ls -lh'
-# List all files, excluding . and ..
-alias la='ls -A'
-# List all files in long format, excluding . and ..
-alias lla='ls -lhA'
-# List only directories
-alias lsd='ls -l | grep --color=never "^d"'
+alias ll='ls -lh' # list all files in long format, with human-readable sizes
+alias l='ll -A' # list all files, excluding . and .. (-A)
+alias lla='ls -lhA' # list all files in long format, excluding . and .. (-A)
+alias lsd='ls -l | grep --color=never "^d"' # list only directories
 
-# Enable colored `grep` and `diff` output
 alias grep='grep --color=auto'
-alias egrep='egrep --color=auto'
-alias pgrep='pgrep --color=auto'
 alias diff='diff --color=auto'
 
-# Use Nord theme by default in `bat`
 has bat && alias bat='\bat --theme=Nord'
-# Add a cute cat to the cat command
-alias cat="$DOTS_HOME/scripts/system/cat"
+alias cat="$DOTS_HOME/scripts/system/cat" # make cat command an actual cat
 
-# View the current working tree status using the short format
-alias gst='git status -s'
-# View abbreviated SHA, description, and history graph of the latest 20 commits
+alias gst='git status --short'
 alias gl='git log --pretty=oneline -n 20 --graph --abbrev-commit'
-# Stage all changes in the working directory
-alias gaa='git add -A'
-# Git add shortcut
 alias ga='git add'
-# Easy restore a staged file
 alias grs='git restore --staged'
-# Show the diff between the latest commit and the current state
-alias gdf='git diff-index --quiet HEAD -- || clear; git --no-pager diff --patch-with-stat'
-# Simpler command to list all branches
-alias gbr='git branch -avv'
-# Other Git shortcuts
 alias gpl='git pull'
 alias gps='git push'
-alias gnuke='git clean -df && git reset --hard'
 
-# Use $XINITRC variable in `startx` if `xinitrc` file exists
-[ -f "$XINITRC" ] && alias startx="startx $XINITRC"
+# use $XINITRC variable in `startx` if `xinitrc` file exists
+[[ -f "$XINITRC" ]] && alias startx="startx $XINITRC"
