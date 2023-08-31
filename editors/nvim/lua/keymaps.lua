250aa36e107fee9f5cd1740ef55578fdaa81a261
Rix Silverith <71402816+rixsilverith@users.noreply.github.com>
2023-09-01 01:15:09 +0200

    Replace visual selection/search result with keybinding in neovim

--- editors/nvim/lua/keymaps.lua
+++ editors/nvim/lua/keymaps.lua
@@ -1,60 +1,56 @@
-local keymap = function(mode, shortcut, command)
-  vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = true, silent = true })
+local keymap = function(mode, keys, action, opts)
+    opts = opts or { noremap = true, silent = true }
+    vim.api.nvim_set_keymap(mode, keys, action, opts)
 end
 
 vim.g.mapleader = ' '
 
-keymap('n', '<leader>w', ':up<cr>') -- save buffer only if modified
-keymap('n', '<leader>wq', ':wq<cr>')
+keymap('i', '<Esc>', '<nop>') -- disable <Esc> on insert mode
+keymap('n', '<cr>', '<nop>') -- disable <enter> on normal mode
+
 keymap('n', '<leader>q', ':q<cr>')
+keymap('n', '<leader>wq', ':wq<cr>')
+keymap('n', '<leader>w', ':up<cr>') -- save buffer only if modified
 keymap('n', '<leader>bq', ':bd<cr>') -- close current buffer
-keymap('n', '<leader>fs', ':Telescope git_files<cr>')
-keymap('n', '<leader>ff', ':Telescope find_files<cr>')
+
+keymap('i', 'jk', '<Esc>') -- fast insert -> normal mode
+
+-- replace visual selection
+keymap('x', '<leader>ss', "y:%s/<C-r>0//gI<Left><Left><Left>", { noremap = true })
+-- replace search result
+keymap('n', '<leader>ss', [[:%s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>]], { noremap = true })
+
+keymap('n', 'J', 'mzJ`z') -- prevent cursor movement upon joining lines
+keymap('x', 'p', '"_dP') -- prevent yank register override upon pasting
+keymap({ 'n', 'v' }, '<leader>d', '"_d') -- delete to void register
+
+keymap('n', '<leader>dg', ':ToggleDiag<cr>') -- LSP diagnostics toggle
+keymap('n', '<leader>csh', ':noh<cr>') -- unhighlight search
 
 -- move selected lines around
 keymap('v', 'J', ":m '>+1<CR>gv=gv")
 keymap('v', 'K', ":m '<-2<CR>gv=gv")
 
--- make cursor stay in position when joining lines
-keymap('n', 'J', 'mzJ`z')
-
--- prevent copy buffer override when pasting
-keymap('x', '<leader>p', "\"_dP")
--- delete to void register
-keymap('n', '<leader>d', "\"_d")
-keymap('v', '<leader>d', "\"_d")
+-- next/previous search result, but center it vertically
+keymap('n', 'n', 'nzzzv')
+keymap('n', 'N', 'Nzzzv')
 
 -- keep cursor centered when moving half-page up and down
 keymap('n', '<C-d>', '<C-d>zz')
 keymap('n', '<C-u>', '<C-u>zz')
 
-keymap('n', '<leader>tv', ':botright vnew <Bar> :terminal<cr>')
-keymap('n', '<leader>th', ':botright new <Bar> :terminal<cr>')
--- keymap('n', '<leader>cc', ':sp | terminal make run_% <cr>')
-keymap('t', '<Esc>', '<C-\\><C-n>')
-keymap('t', 'jk', '<Esc>')
-
--- Use ctrl-[hjkl] to move to between splits
+-- use ctrl-[hjkl] to move to between splits
 keymap('n', '<C-h>', ':wincmd h<cr>')
 keymap('n', '<C-j>', ':wincmd j<cr>')
 keymap('n', '<C-k>', ':wincmd k<cr>')
 keymap('n', '<C-l>', ':wincmd l<cr>')
 
-keymap('i', 'jk', '<Esc>')
-keymap('i', '<Esc>', '<nop>') -- Disable <Esc> on insert mode
-keymap('n', '<cr>', '<nop>') -- Disable <enter> on normal mode
+keymap('n', '<leader>fs', ':Telescope git_files<cr>')
+keymap('n', '<leader>ff', ':Telescope find_files<cr>')
 
--- Edit neovim configuration in a vertical split
-keymap('n', '<leader>ev', ':vsplit $MYVIMRC<cr>')
--- Reload neovim configuration
-keymap('n', '<leader>sv', ':source $MYVIMRC<cr>')
+keymap('n', '<leader>tv', ':botright vnew <Bar> :terminal<cr>')
+keymap('n', '<leader>th', ':botright new <Bar> :terminal<cr>')
 
--- Go to next or previous search and center the result
-keymap('n', 'n', 'nzzzv')
-keymap('n', 'N', 'Nzzzv')
-
--- LSP diagnostics toggle bindings
-keymap('n', '<leader>dg', '<cmd> ToggleDiag <cr>')
-
-keymap('n', '<leader>csh', ':noh<cr>')
+keymap('t', '<Esc>', '<C-\\><C-n>')
+keymap('t', 'jk', '<Esc>')
 
