69b65576a3bd26726c6afe2b632680b2a9dc8eaf
Rix Silverith <71402816+rixsilverith@users.noreply.github.com>
2024-05-27 13:59:33 +0200

    Source exported environment variables before shell functions

--- shell/init.sh
+++ shell/init.sh
@@ -1,6 +1,6 @@
 # `init.sh` allows sharing functions, aliases and almost all environment
 # variables across the bash and zsh shells.
 
-source "$DOTS_HOME/shell/functions.sh"
 source "$DOTS_HOME/shell/exports.sh"
+source "$DOTS_HOME/shell/functions.sh"
 source "$DOTS_HOME/shell/aliases.sh"
