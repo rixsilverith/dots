b2d045ead6d9a35d2a17849bafb1d4ea7172fb83
Rix Silverith <71402816+rixsilverith@users.noreply.github.com>
2024-05-27 20:33:08 +0200

    Improve shell history configuration

--- shell/history.zsh
+++ shell/history.zsh
@@ -1,9 +1,15 @@
 #!/usr/bin/zsh
 
-setopt HIST_IGNORE_ALL_DUPS  # Remove older commands from the history if a duplicate is to be added
-setopt HIST_FCNTL_LOCK
+setopt APPEND_HISTORY  # zsh sessions append their history to the history file, rather than replace it
+setopt HIST_FCNTL_LOCK  # use fcntl for history file locking
+setopt HIST_FIND_NO_DUPS  # dont show duplicate history entries on find
+setopt HIST_IGNORE_ALL_DUPS  # remove older commands from the history if a duplicate is to be added
+setopt HIST_IGNORE_DUPS  # dont insert duplicates in history
+setopt HIST_IGNORE_SPACE  # ignore typed commands when prepended with a space
+setopt HIST_SAVE_NO_DUPS  # dont save duplicates to history file
+setopt SHARE_HISTORY  # share history between sessions
 
 HISTFILE="${XDG_DATA_HOME:-$HOME/.local/share}/history"
-HISTSIZE=1000
-SAVEHIST=1000
-
+HISTSIZE=5000
+SAVEHIST=$HISTSIZE
+HISTUP=erase
